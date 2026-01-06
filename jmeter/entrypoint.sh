cbb73678af2453fbffbd85e2b752cec9b8d15fbc
anton.sermus <anton.sermus@rt.ru>
2026-01-06 12:43:17 +0300

    Add dockerfile for jmeter

---
+++ jmeter/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+# Ожидаем запуск тестируемого сервиса 10 секунд
+/usr/local/bin/wait-for-it.sh $TEST_SERVICE --timeout=10 --strict -- echo "$TEST_SERVICE is up!"
+
+jmeter -n -t $PATH_TO_TEST_FILE
+
+exec "$@"
