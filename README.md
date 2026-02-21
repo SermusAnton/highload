d824172eea445bc6c9a39cac5f93569d9aa8ec3a
anton.sermus <anton.sermus@rt.ru>
2026-02-21 17:54:19 +0300

    Delete redis

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ Highload Architect
 БД PostgresQL, по-умолчанию: localhost:5432
 1. Выполнить из каталога проекта 
 docker compose up
-2. Зайти в grafana (admin:admin) Dashboards -> JMeter
+2. Зайти в grafana http://localhost:3000/ (admin:admin) Dashboards -> JMeter
 
 Postman коллекция \json\OTUS Highload Architect.postman_collection.json
--- src/main/java/com/highload/backend/service/FriendService.java
+++ src/main/java/com/highload/backend/service/FriendService.java
@@ -1,43 +1,24 @@
 package com.highload.backend.service;
 
 import com.highload.backend.dao.FriendRepository;
-import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Service;
 
-import java.util.Set;
 import java.util.UUID;
 
 @Service
 public class FriendService {
 
     private final FriendRepository friendRepository;
-    private final RedisService redisService;
 
-    public final String FRIENDS_DICTIONARY_NAME = "Friends";
-
-    public FriendService(FriendRepository friendRepository,
-        RedisService redisService) {
+    public FriendService(FriendRepository friendRepository) {
         this.friendRepository = friendRepository;
-        this.redisService = redisService;
-    }
-
-    @PostConstruct
-    public void init() {
-        var friends = friendRepository.getAll();
-        redisService.putAllInHash(FRIENDS_DICTIONARY_NAME, friends);
     }
 
     public void add(UUID userId, UUID friendId) {
         friendRepository.add(userId, friendId);
-        redisService.addValueToSetInHash(FRIENDS_DICTIONARY_NAME, userId, friendId);
     }
 
     public void delete(UUID userId, UUID friendId) {
         friendRepository.delete(userId, friendId);
-        redisService.deleteValueFromSetInHash(FRIENDS_DICTIONARY_NAME, userId, friendId);
-    }
-
-    public Set<UUID> getFriendsBy(UUID userId) {
-        return redisService.getAllFromHash(FRIENDS_DICTIONARY_NAME, userId);
     }
 }
